a19c96f40218e77f1c2da28d7fd5912efd1714d8
Filipe Pina <fopina@gmail.com>
2016-08-09 16:16:16 +0100

    brilliant strategy 1

--- strategy1.py
+++ strategy1.py
@@ -24,57 +24,71 @@ kkthnx
 
 import satoshiminesbot
 import time
+import sys
 
 MINES = 1
 GUESSES = 1
-BETS = [30, 751, 18776, 469401]
+MULTIPLIER = 25
+INITIAL_BET = 30
 
 
 def main(player_hash, initial_balance=0):
     bot = satoshiminesbot.SMB(player_hash)
     wins, losses = 0, 0
+    if not initial_balance:
+        initial_balance = bot.balance
     balance = int(initial_balance)
 
-    bet_ind = 0
+    bet = INITIAL_BET
 
     try:
         while True:
             done = True
-            bet = BETS[bet_ind]
 
             if initial_balance and bet > balance:
-                print('Not enough balance for this bet...')
+                sys.stderr.write('Not enough balance for this bet...\n')
                 exit(1)
 
             try:
                 g = bot.new_game(bet, MINES)
-            except Exception as e:
-                # unexpected error, let's ignore and try again
-                print(e)
-                time.sleep(5)
+            except satoshiminesbot.SMBError as e:
+                if e[0] != 90:
+                    raise
+                # network error, just retry
+                time.sleep(3)
                 continue
             bits = 0
             for guess in xrange(GUESSES):
-                f = g.play()
+                try:
+                    f = g.play()
+                except satoshiminesbot.SMBError as e:
+                    if e[0] != 90:
+                        raise
+                    # network error, just retry
+                    time.sleep(3)
+                    continue
                 if f['outcome'] == 'bomb':
                     balance -= bet
                     losses += 1
                     print('- %s bits - BAL: %d - %s' % (bet, balance, g.url()))
-                    bet_ind += 1
-                    if bet_ind >= len(BETS):
-                        print('You should have cut your losses sooner....')
-                        exit(1)
+                    bet = bet * MULTIPLIER + 1
                     done = False
                     break
                 elif f['outcome'] == 'bitcoins':
-                    bet_ind = 0
+                    bet = INITIAL_BET
                     bits += int(f['change'] * 1000000)
                 else:
-                    print('wtf?!')
-                    print(f)
-                    raise Exception('STOP!')
+                    raise Exception('STOP! WTF?', f)
             if done:
-                g.cashout()
+                try:
+                    g.cashout()
+                except satoshiminesbot.SMBError as e:
+                    sys.stderr.write('missed a cashout for %s\n' % g._info['game_hash'])
+                    if e[0] != 90:
+                        raise
+                    # network error, try again otherwise we miss the cashout!!!
+                    time.sleep(3)
+                    g.cashout()
                 balance += bits
                 wins += 1
                 print('+ %s bits - BAL: %d - %s' % (bits, balance, g.url()))
@@ -86,8 +100,6 @@ def main(player_hash, initial_balance=0):
 
 
 if __name__ == '__main__':
-    import sys
-
     if len(sys.argv) < 2:
         sys.stderr.write('Usage: %s PLAYER_HASH\n' % __file__)
         sys.exit(1)
